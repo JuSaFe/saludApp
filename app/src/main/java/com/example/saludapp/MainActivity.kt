4904ee1fe235b4a0813a189f21e063cab1fcab2a
Juansanflx <jcotolisanfelix@gmail.com>
2023-11-07 11:55:37 +0100

    GreetingActivity

--- app/src/main/java/com/example/saludapp/MainActivity.kt
+++ app/src/main/java/com/example/saludapp/MainActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatEditText
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,7 +16,14 @@ class MainActivity : AppCompatActivity() {
         val etName:AppCompatEditText = findViewById(R.id.etName)
         btnHello.setOnClickListener {
             val name = etName.text.toString()
-            Toast.makeText(this, "Hola $name!", Toast.LENGTH_LONG).show()
+            if (name.isNotEmpty()) {
+                Snackbar.make(btnHello, "Hola $name!", Snackbar.LENGTH_SHORT)
+                    .setAnchorView(btnHello)
+                    .setAction("RESET") { etName.setText(null)}
+                    .show()
+            } else {
+                Toast.makeText(this, "Introduce un nombre", Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
